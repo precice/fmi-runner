4d7cab2d2c608e2b84c9a72a1abacd9997c60e3f
Leonard Willeke <st150067@stud.uni-stuttgart.de>
2023-05-18 18:28:56 +0200

    Adapt test setup and git workflow to work as regression test and minimal example

--- tests/test.py
+++ tests/test.py
@@ -1,18 +1,17 @@
-import pandas as pd
-
+from numpy import genfromtxt
 
 # Expected final results
 expected_solver_one = 9
 expected_solver_two = 10
 
 # Calculated results
-df_solver_one = pd.read_csv("./SolverOne/output/test-SolverOne.csv")
-df_solver_two = pd.read_csv("./SolverTwo/output/test-SolverTwo.csv")
+data_solver_one = genfromtxt('./SolverOne/output/test-SolverOne.csv', delimiter=',', skip_header=1)
+data_solver_two = genfromtxt('./SolverTwo/output/test-SolverTwo.csv', delimiter=',', skip_header=1)
 
-calc_solver_one = df_solver_one["write_data"][6]
-calc_solver_two = df_solver_two["write_data"][6]
+calc_solver_one = data_solver_one[6,1]
+calc_solver_two = data_solver_two[6,1]
 
 assert expected_solver_one == calc_solver_one, "Results of SolverOne are wrong"
 assert expected_solver_two == calc_solver_two, "Results of SolverTwo are wrong"
 
-print("Test finished succesfully")
+print("Test finished successfully")
