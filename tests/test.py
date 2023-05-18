2be8a3667f904562510560933eca808213eec967
Leonard Willeke <st150067@stud.uni-stuttgart.de>
2023-05-18 18:52:18 +0200

    Run automated tests only for main branch

--- tests/test.py
+++ tests/test.py
@@ -8,8 +8,8 @@ expected_solver_two = 10
 data_solver_one = genfromtxt('./SolverOne/output/test-SolverOne.csv', delimiter=',', skip_header=1)
 data_solver_two = genfromtxt('./SolverTwo/output/test-SolverTwo.csv', delimiter=',', skip_header=1)
 
-calc_solver_one = data_solver_one[6,1]
-calc_solver_two = data_solver_two[6,1]
+calc_solver_one = data_solver_one[6, 1]
+calc_solver_two = data_solver_two[6, 1]
 
 assert expected_solver_one == calc_solver_one, "Results of SolverOne are wrong"
 assert expected_solver_two == calc_solver_two, "Results of SolverTwo are wrong"
