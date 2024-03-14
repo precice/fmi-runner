0e86ea136185b57c6fb82c469dcd9a0a47654192
Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>
2024-03-14 17:01:54 +0100

    Remove setup.py content

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-import os
 from setuptools import setup
 
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-setup(
-    name='fmiprecice',
-    version='0.1.0',
-    author="Leonard Willeke",
-    author_email="st150067@stud.uni-stuttgart.de",
-    description="A tool to co-simulate FMU models with the coupling library preCICE.",
-    license="LGPL-3.0",
-    url="https://github.com/precice/fmi-runner",
-    packages=['fmiprecice'],
-    long_description=read('README.md'),
-    python_requires=">=3.0",
-    install_requires=[
-        'pyprecice>=2.0',
-        'fmpy',
-        'numpy',
-    ],
-    entry_points={
-        'console_scripts': ['fmiprecice=fmiprecice.runner:main']},
-)
+setup()
